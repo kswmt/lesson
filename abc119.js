67c212ea4e2987a103808cafe54ee0fbe1a69bbb
kswmt <41228169+kswmt@users.noreply.github.com>
2019-02-25 18:29:53 +0900

    Create abc119.js

---
+++ abc119.js
@@ -0,0 +1,14 @@
+
+     function Main(input) {
+    input = input.split('/');
+
+    let ms = Date.parse(`${input[0]}-${input[1]}-${input[2]}`);
+
+    if (ms < 1556668800000) {
+        return console.log('Heisei');
+    } else {
+        return console.log('TBD');
+    }
+}
+
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
